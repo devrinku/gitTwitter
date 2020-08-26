65d2283822713c0814f0b4b690fa5dd4f031781f
devrinku <devrinku1290@gmail.com>
2020-08-27 00:49:00 +0530

    fix

--- server.js
+++ server.js
@@ -65,12 +65,7 @@ app.use(errorHandler);
 
 //Serving static assets for production
 
-// Set static folder
-app.use(express.static("client/build"));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+app.use(express.static(path.join(__dirname, "client/build")));
 
 //Starting Server
 const PORT = process.env.PORT || 5000;
