3ba182faa6521a72c2da3a177503e01b0684f928
devrinku <devrinku1290@gmail.com>
2020-08-27 12:22:20 +0530

    deploy

--- server.js
+++ server.js
@@ -65,11 +65,14 @@ app.use(errorHandler);
 
 //Serving static assets for production
 
-app.use(express.static("client/build"));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 //Starting Server
 const PORT = process.env.PORT || 5000;
 const server = app.listen(
