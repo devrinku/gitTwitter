89842902ba574b05c228bc00dc082aa999138834
devrinku <devrinku1290@gmail.com>
2020-08-27 17:00:16 +0530

    fixed

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.use("/api/v1/post", post);
 app.use(errorHandler);
 
 //Serving static assets for production
-if (process.env.NODE_ENV === " production ") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
